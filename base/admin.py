7ccc70bd3c6727b610ec57f8667f61dfcba92e1f
kumar-aadarsh0001 <naughtynbita0420@gmail.com>
2023-01-24 14:13:50 +0530

    first commit

---
+++ base/admin.py
@@ -0,0 +1,10 @@
+# from django.contrib import admin
+
+# # Register your models here.
+
+# from .models import RoomMember
+
+# @admin.register(RoomMember)
+
+# class UserAdmin(admin.ModelAdmin):
+#     list_display=[]
